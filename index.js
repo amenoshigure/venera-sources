c6f9b8919d540889103eca9a77f70908e5b426c0
amenoshigure <teekeat41@gmail.com>
2026-07-30 15:59:08 +0800

    Add source list for handling network errors
    This file contains a source list for handling network 403 errors and file type restrictions.

---
+++ index.js
@@ -0,0 +1,9 @@
+// 这是一个专门用来解决网络 403 和文件类型限制的源列表文件
+var sources = [
+  {
+    "name": "嗨皮漫画",
+    "url": "https://cdn.jsdelivr.net/gh/amenoshigure/venera-sources@main/happy.js",
+    "version": "1.0.1",
+    "description": "嗨皮漫画源"
+  }
+];
